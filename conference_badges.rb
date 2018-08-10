7a8b197146ce3fbfbd3f620dcc0d2c17ccd057ad
Beatrice Mendoza <mendoza@nyu.edu>
2018-08-10 02:13:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,6 @@ def batch_badge_creator(array_of_names)
 end
 
 def assign_rooms(array_of_names)
-  array_of_names.each_with_index{ |name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+  assigned_rooms = []
+  array_of_names.each_with_index{ |name, index| assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index}!"}
 end
